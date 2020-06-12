2421367512be060b230ddbb454ae822ca08fbe37
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-12 20:59:00 +0900

    get api data by flatmap and log to return back result

--- mod.ts
+++ mod.ts
@@ -1,10 +1,44 @@
+import * as log from "https://deno.land/std/log/mod.ts";
+import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js"
+
+interface Launch {
+  flightNumber: number;
+  mission: string;
+  rocket: string;
+  customers: Array<string>;
+}
+
+const launches = new Map<number, Launch>();
+
 async function downloadLaunchData() {
+  log.info("Downloading launch data...")
   const response = await fetch("http://api.spacexdata.com/v3/launches", {
     method: "GET"
   });
 
-  const lauchData = await response.json();
-  console.log(lauchData)
+  if (!response.ok) {
+    log.warning("Problem downloading launch data")
+    throw new Error ("Lauch data download failed")
+  }
+
+  const launchData = await response.json();
+  for (const launch of launchData) {
+    const payloads = launch["rocket"]["second_stage"]["payloads"];
+    const customers = _.flatMap(payloads, (payload : any) => {
+      return payload["customers"]
+    })
+
+    const flightData = {
+      flightNumber: launch["flight_number"],
+      mission: launch["mission_name"],
+      rocket: launch["rocket"]["rocket_name"],
+      customers: customers
+    };
+
+    launches.set(flightData.flightNumber, flightData)
+
+    log.info(JSON.stringify(flightData));
+  }
 }
 
 await downloadLaunchData();
